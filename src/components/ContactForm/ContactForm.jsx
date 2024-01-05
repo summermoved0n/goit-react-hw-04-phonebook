4d3969a932bb6a70c88c6bdaa7b0f1a675681023
summermoved0n <135251802+summermoved0n@users.noreply.github.com>
2024-01-05 00:03:12 -0500

    deleted comments

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -24,8 +24,6 @@ export default function ContactForm({ onSubmit }) {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(name);
-    console.log(number);
 
     onSubmit({ name, number });
     resetSubmit();
@@ -71,69 +69,3 @@ export default function ContactForm({ onSubmit }) {
     </form>
   );
 }
-
-// class ContactForm extends Component {
-//   state = {
-//     name: '',
-//     number: '',
-//   };
-
-//   handleChange = e => {
-//     const { name, value } = e.currentTarget;
-//     this.setState({
-//       [name]: value,
-//     });
-//   };
-
-// handleSubmit = e => {
-//   e.preventDefault();
-
-//   this.props.onSubmit(this.state);
-//   this.resetSubmit();
-// };
-
-// resetSubmit = () => {
-//   this.setState({
-//     name: '',
-//     number: '',
-//   });
-// };
-
-//   render() {
-//     const { name, number } = this.state;
-//     const inputNameId = nanoid();
-//     const inputNumberId = nanoid();
-
-//     return (
-//       <form className={css.form} onSubmit={this.handleSubmit}>
-//         <label htmlFor={inputNameId}>
-//           <span className={css.form_title}>Name</span>
-//           <input
-//             className={css.form_input}
-//             type="text"
-//             name="name"
-//             value={name}
-//             id={inputNameId}
-//             required
-//             onChange={this.handleChange}
-//           />
-//         </label>
-//         <label htmlFor={inputNumberId}>
-//           <span className={css.form_title}>Number</span>
-//           <input
-//             className={css.form_input}
-//             type="tel"
-//             name="number"
-//             value={number}
-//             id={inputNumberId}
-//             required
-//             onChange={this.handleChange}
-//           />
-//         </label>
-//         <button className={css.form_btn} type="submit">
-//           Add contact
-//         </button>
-//       </form>
-//     );
-//   }
-// }
